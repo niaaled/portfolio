3f8d629a15614a02368de29116be64a5c0c9f4f1
niaaled <niaadurna@gmail.com>
2024-12-11 18:50:09 +0100

    Adding menu functions.js

--- calendar.js
+++ calendar.js
@@ -76,3 +76,14 @@ function closePopup() {
     const popup = document.getElementById('popup');
     popup.style.display = 'none';
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.getElementById('menuToggle');
+  const navMenu = document.getElementById('navMenu');
+
+  menuToggle.addEventListener('click', function() {
+    navMenu.classList.toggle('active');
+  });
+});
